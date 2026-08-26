84b261a2ac9d3d1e2175fd13d9569f4f66eea943
srashti-blocks <sgsrashtigupta45@gmail.com>
2026-08-26 09:58:46 +0530

    Add AnimeNFT contract and character art

---
+++ Contracts/anime-nft-marketplace.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AnimeNFT is ERC721, Ownable {
+    uint256 public nextTokenId;
+    uint256 public constant MAX_SUPPLY = 8;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC721("JJK Anime NFT", "JJKNFT") Ownable(msg.sender) {}
+
+    function mint(string memory metadataURI) public {
+        require(nextTokenId < MAX_SUPPLY, "All characters minted");
+        uint256 tokenId = nextTokenId;
+        _safeMint(msg.sender, tokenId);
+        _tokenURIs[tokenId] = metadataURI;
+        nextTokenId++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_ownerOf(tokenId) != address(0), "Token does not exist");
+        return _tokenURIs[tokenId];
+    }
+}
